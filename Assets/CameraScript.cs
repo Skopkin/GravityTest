38793711824aaa460156721e950b49961f48ac38
John Chapman <j.p.chapman1@gmail.com>
2017-04-05 18:48:19 -0400

    initial commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject player;
+	private float cameraOffset;
+	// Use this for initialization
+	void Awake() {
+		cameraOffset = transform.position.x - player.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	transform.position = new Vector3 (player.transform.position.x + cameraOffset, transform.position.y, -10);
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private SpriteRenderer spriteRenderer;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		rb.gravityScale = speed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate(.05f,0,0);
+
+		if (Input.GetMouseButtonDown (0)) {
+			rb.gravityScale *= -1f;
+			if (rb.gravityScale < 0) {
+				spriteRenderer.flipY = true;
+			} else
+			spriteRenderer.flipY = false;
+
+		}
+	}
+
+}
